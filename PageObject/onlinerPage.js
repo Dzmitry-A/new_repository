d927edfeed6427f4aa3ed0f4ee4395e5409b70a9
Dzmitry Abramuk <Abramuk08@mail.ru>
2021-04-09 09:39:42 +0300

    work with basic controls Protractor

---
+++ PageObject/onlinerPage.js
@@ -0,0 +1,42 @@
+let onlinerPage =  function() {
+    let checkBoxApple = element(by.cssContainingText('.schema-filter__checkbox-text','Apple'));
+    let searchInput = element(by.css('.fast-search__input'));
+    
+    this.openSite = async function() {
+        browser.waitForAngularEnabled(false);
+        await browser.get('https://catalog.onliner.by/mobile');
+    };
+   
+    this.clickCheckBox = async function() {
+        await browser.wait(protractor.ExpectedConditions.presenceOf(checkBoxApple), 5000);
+        await checkBoxApple.click(); 
+    };
+
+    this.scrollPage = async function() {
+        await browser.executeScript('window.scrollTo(0, document.body.scrollHeight)');
+        await browser.sleep(3000);
+        await browser.executeScript('window.scrollTo(0, -document.body.scrollHeight)')
+    };
+
+    this.clickOnTheDropdown = async function(text) {
+        await browser.wait(protractor.ExpectedConditions.presenceOf(searchInput), 5000);
+        await searchInput.click().sendKeys(text); 
+        await browser.sleep(3000);
+        let pop_upList = element(by.css('.search__result:nth-child(3)'));
+        await browser.wait(protractor.ExpectedConditions.presenceOf(pop_upList), 5000);
+	    await pop_upList.sendKeys(protractor.Keys.ENTER);
+
+    };
+
+    this.getTextInput = async function(text) {
+        await browser.wait(protractor.ExpectedConditions.presenceOf(searchInput), 5000);
+        await searchInput.click().sendKeys(text); 
+        return await searchInput.getAttribute('value');
+    };
+
+    this.getAttributeInput = async function() {
+        return await searchInput.getAttribute('class');
+    };
+};
+
+module.exports = new onlinerPage ();
